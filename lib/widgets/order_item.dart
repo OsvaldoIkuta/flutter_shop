58ae9d398712e028a088285b2557fa5fca823388
Osvaldo Massaharu Ikuta <osvaldoikuta@MacBook-Pro-de-Osvaldo.local>
2020-05-31 20:53:52 -0300

    finalizado o modulo de animacoes

--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -19,50 +19,55 @@ class _OrderItemState extends State<OrderItem> {
   @override
   Widget build(BuildContext context) {
     final products = widget.order.products;
-    return Card(
-      margin: EdgeInsets.all(10),
-      child: Column(children: <Widget>[
-        ListTile(
-          title: Text('\$${widget.order.amount}'),
-          subtitle: Text(DateFormat('dd MM yyyy hh:mm').format(widget.order.dateTime) ),
-          trailing: IconButton(
-            icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more), 
-            onPressed: () {
-              setState(() {
-                _expanded = !_expanded;
-              });
-            }),
-        ),
-        if (_expanded) Container(
-          padding: EdgeInsets.symmetric(
-            horizontal: 15,
-            vertical: 4
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 300),
+      height: _expanded ? min(products.length * 20.0 + 110, 200) : 95,
+          child: Card(
+        margin: EdgeInsets.all(10),
+        child: Column(children: <Widget>[
+          ListTile(
+            title: Text('\$${widget.order.amount}'),
+            subtitle: Text(DateFormat('dd MM yyyy hh:mm').format(widget.order.dateTime) ),
+            trailing: IconButton(
+              icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more), 
+              onPressed: () {
+                setState(() {
+                  _expanded = !_expanded;
+                });
+              }),
           ),
-          height: min(products.length * 20.0 + 10.0, 100),
-          child: ListView.builder(
-            itemCount: products.length,
-            itemBuilder: (ctx, index) => Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Text(
-                    products[index].title,
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
+          AnimatedContainer(
+            duration: Duration(milliseconds: 300),
+            padding: EdgeInsets.symmetric(
+              horizontal: 15,
+              vertical: 4
+            ),
+            height: _expanded ? min(products.length * 20.0 + 10, 100) : 0,
+            child: ListView.builder(
+              itemCount: products.length,
+              itemBuilder: (ctx, index) => Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(
+                      products[index].title,
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                  ),
-                  Text(
-                    '${products[index].quantity}X \$${products[index].price}',
-                    style: TextStyle(
-                      fontSize: 18,
-                      color: Colors.grey
-                    ),
-                  )
-                ],
-              )
-          ),
-        ) 
-      ],),
+                    Text(
+                      '${products[index].quantity}X \$${products[index].price}',
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.grey
+                      ),
+                    )
+                  ],
+                )
+            ),
+          ) 
+        ],),
+      ),
     );
   }
 }
--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -20,58 +20,59 @@ class ProductItem extends StatelessWidget {
     final authData = Provider.of<Auth>(context);
 
     return ClipRRect(
-        borderRadius: BorderRadius.circular(10),
-        child: GridTile(
-          child: GestureDetector(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        child: GestureDetector(
             onTap: () {
-              Navigator.of(context).pushNamed(
-                ProductDetailScreen.routeName, 
-                arguments: product.id
-              );
+              Navigator.of(context).pushNamed(ProductDetailScreen.routeName,
+                  arguments: product.id);
             },
-            child: Image.network(
-              product.imageUrl,
-              fit: BoxFit.cover,
-            ),
-          ),
-          footer: GridTileBar(
-            backgroundColor: Colors.black87,
-            leading: Consumer<Product>(
-              builder: (ctx, product, _) =>  IconButton(
-                icon: Icon(product.isFavorite == true ? Icons.favorite : Icons.favorite_border),
-                onPressed: () {
-                  product.toggleFavoriteStatus(authData.token, authData.userId);
-                },
-                color: Theme.of(context).accentColor,
+            child: Hero(
+              tag: product.id,
+              child: FadeInImage(
+                placeholder:
+                    AssetImage('assets/images/product-placeholder.png'),
+                image: NetworkImage(product.imageUrl),
+                fit: BoxFit.cover,
               ),
-            ),
-            trailing: IconButton(
-              icon: Icon(Icons.shopping_cart),
+            )),
+        footer: GridTileBar(
+          backgroundColor: Colors.black87,
+          leading: Consumer<Product>(
+            builder: (ctx, product, _) => IconButton(
+              icon: Icon(product.isFavorite == true
+                  ? Icons.favorite
+                  : Icons.favorite_border),
               onPressed: () {
-                cart.addItem(product.id, product.price, product.title);
-                Scaffold.of(context).hideCurrentSnackBar();
-                Scaffold.of(context).showSnackBar(
-                  SnackBar(
-                    content: Text('Added item to a cart!'),
-                    duration: Duration(seconds: 2),
-                    backgroundColor: Colors.green,
-                    action: SnackBarAction(
-                      label: 'UNDO', 
-                      onPressed: () {
-                        cart.removeSingleItem(product.id);
-                      }
-                    ),
-                  )
-                );
+                product.toggleFavoriteStatus(authData.token, authData.userId);
               },
               color: Theme.of(context).accentColor,
             ),
-            title: Text(
-              product.title,
-              textAlign: TextAlign.center,
-            ),
+          ),
+          trailing: IconButton(
+            icon: Icon(Icons.shopping_cart),
+            onPressed: () {
+              cart.addItem(product.id, product.price, product.title);
+              Scaffold.of(context).hideCurrentSnackBar();
+              Scaffold.of(context).showSnackBar(SnackBar(
+                content: Text('Added item to a cart!'),
+                duration: Duration(seconds: 2),
+                backgroundColor: Colors.green,
+                action: SnackBarAction(
+                    label: 'UNDO',
+                    onPressed: () {
+                      cart.removeSingleItem(product.id);
+                    }),
+              ));
+            },
+            color: Theme.of(context).accentColor,
+          ),
+          title: Text(
+            product.title,
+            textAlign: TextAlign.center,
           ),
         ),
+      ),
     );
   }
 }
